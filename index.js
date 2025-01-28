70c530f7c0b339b6d2c8aeec9d899299ba5225df
Juan <juan_41629456@hotmail.com>
2025-01-28 07:58:55 -0300

    primerCommitPreEntrega

---
+++ index.js
@@ -0,0 +1,16 @@
+/*import http from 'http'
+ 
+const server = http.createServer((req , res)=>{
+    res.end(`mi primer hola con http`)
+})
+server.listen(8080,()=>{
+    console.log(`Servedir on 8080`)
+})
+*/
+let idrandom = {}
+
+for(let i=1 ; i <= 20; i++){
+    const numeroRandom = Math.round(Math.random()* 20)
+    idrandom [numeroRandom] = 0
+}
+ 
